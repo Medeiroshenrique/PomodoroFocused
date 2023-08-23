a9796b438364a70933acc89eda34b356a7690558
Henrique  Medeiros <102472768+Medeiroshenrique@users.noreply.github.com>
2023-08-23 13:02:03 -0300

    :feat: including the pomodoro visual timer

--- Pomodoro/Form1.cs
+++ Pomodoro/Form1.cs
@@ -4,35 +4,47 @@ namespace Pomodoro
 {
     public partial class Form1 : Form
     {
+        //Important information: 25 minutes has 1500 seconds
+        private int Worktime = 25 * 60;
+        private int RestTime = 5 * 60;
+        private int Minutes = 25;
+        private int Seconds = 60;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        //Important information: 25 minutes has 1500 seconds
-        int Worktime = 10; //* 60;
-        int RestTime = 5;//* 60;
-        
-
-        private void PomodoroTimer_Tick(object sender, EventArgs e)
-        {
-            if (Worktime > 0)
-            {
-                Worktime -= 1;
-            }
-            else
-            {
-                PomodoroTimer.Stop();
-                MessageBox.Show("Time to rest soldier!");
-                RestTimer.Start();
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("and... START YOUR TASK!!!");
             PomodoroTimer.Start();
+            Minutes -= 1;
+        }
 
+        private void PomodoroTimer_Tick(object sender, EventArgs e)
+        {
+            if (Worktime > 0)
+            {
+                Worktime -= 1;
+                Seconds -= 1;
+                this.TimerLabel.Text = $"{Minutes} : {Seconds}";
+                if (Minutes > 0) {
+                    if (Seconds == 0)
+                    {
+                        Minutes -= 1;
+                        Seconds = 60;
+                    }
+                }
+                
+            }
+            else
+            {
+                PomodoroTimer.Stop();
+                MessageBox.Show("Time to rest soldier!");
+                RestTimer.Start();
+                Minutes = 5;
+            }
         }
 
         private void RestTimer_Tick(object sender, EventArgs e)
